854025969ecc427acbea72fbe5e30e64b9a8951c
AleksandrZaec <zaecam95@mail.ru>
2024-07-04 11:56:44 +0300

    habits tests

--- config/settings.py
+++ config/settings.py
@@ -81,17 +81,27 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('BD_USER'),
-        'PASSWORD': os.getenv('BD_PASSWORD'),
-        'HOST': os.getenv('HOST'),
-        'PORT': os.getenv('PORT'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('BD_USER'),
+#         'PASSWORD': os.getenv('BD_PASSWORD'),
+#         'HOST': os.getenv('HOST'),
+#         'PORT': os.getenv('PORT'),
+#     }
+# }
 
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'courseworkdrf',
+       'USER': 'postgres',
+       'PASSWORD': 'Postgres',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
+}
 
 
 # Password validation
